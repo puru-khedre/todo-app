0794dd9931cc8f31004dfe15671d0f2f0f08ab8d
Purushottam khedre <purushottamkhedre123@gmail.com>
2022-04-21 17:57:45 +0530

    creating basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("Hello worlds");
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`server started at url http://localhost:${port}`);
+});
